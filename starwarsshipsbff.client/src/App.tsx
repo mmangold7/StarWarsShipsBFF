e1cdfa69b41ec032a9df1976d9c3edcfbd9cbfa4
Matt Mangold <mmangold7@gmail.com>
2025-07-31 22:32:00 -0400

    converted template's weatherforecast page to display starship data using an interface that matches the dto. list works, needs filtering by make with a select

--- starwarsshipsbff.client/src/App.tsx
+++ starwarsshipsbff.client/src/App.tsx
@@ -1,38 +1,52 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+interface Starship {
+  name: string;
+  model: string;
+  starship_class: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  crew: string;
+  passengers: string;
+  max_atmosphering_speed: string;
+  hyperdrive_rating: string;
+  MGLT: string;
+  cargo_capacity: string;
+  consumables: string;
+  films: string[];
+  pilots: string[];
+  url: string;
+  created: string;
+  edited: string;
 }
 
 function App() {
-    const [forecasts, setStarships] = useState<Forecast[]>();
+    const [starships, setStarships] = useState<Starship[]>();
 
     useEffect(() => {
         populateStarshipsData();
     }, []);
 
-    const contents = forecasts === undefined
+    const contents = starships === undefined
         ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
         : <table className="table table-striped" aria-labelledby="tableLabel">
             <thead>
                 <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
+                    <th>Name</th>
+                    <th>Model</th>
+                    <th>Class</th>
+                    <th>Manufacturer</th>
                 </tr>
             </thead>
             <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
+                {starships.map(starship =>
+                    <tr key={starship.name}>
+                        <td>{starship.name}</td>
+                        <td>{starship.model}</td>
+                        <td>{starship.starship_class}</td>
+                        <td>{starship.manufacturer}</td>
                     </tr>
                 )}
             </tbody>
